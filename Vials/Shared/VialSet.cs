fe7d823ff23dfc941b2f7071bd564f6df95fd312
Mikael Lindh <mslindh@gmail.com>
2021-05-19 16:57:47 +0200

    Added complete condition on vial set.

--- Vials/Shared/VialSet.cs
+++ Vials/Shared/VialSet.cs
@@ -7,5 +7,21 @@ namespace Vials.Shared
     public class VialSet : IVialSet
     {
         public IEnumerable<Vial> Vials { get; set; }
+
+        public bool IsComplete 
+        { 
+            get 
+            { 
+                foreach(var vial in Vials)
+                {
+                    if(!(vial.IsComplete || vial.IsEmpty))
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            } 
+        }
     }
 }
