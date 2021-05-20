6efd11914055f697da682fa2715510419e7ff6a7
Mikael Lindh <mslindh@gmail.com>
2021-05-20 11:25:01 +0200

    Added history manager.

---
+++ Vials.Shared.UnitTests/VialSetHistoryTests.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vials.Shared.UnitTests
+{
+    [TestClass]
+    public class VialSetHistoryTests
+    {
+        [TestMethod]
+        public void HistoryIsStacked()
+        {
+            var history = new VialSetHistory();
+
+            history.Store(new VialSet() { Vials = new Vial[] { new Vial(new Color[] { Color.Red }) } });
+            history.Store(new VialSet() { Vials = new Vial[] { new Vial(new Color[] { Color.Red, Color.Blue }) } });
+            history.Store(new VialSet() { Vials = new Vial[] { new Vial(new Color[] { Color.Red, Color.Blue, Color.Green }) } });
+
+            history.Current.Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red, Color.Blue, Color.Green });
+
+            history.GetPrevious().Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red, Color.Blue });
+            history.Current.Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red, Color.Blue });
+
+            history.GetPrevious().Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red });
+            history.Current.Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red });
+
+            history.GetPrevious().ShouldBeNull();
+            history.Current.Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red });
+        }
+
+        [TestMethod]
+        public void NextCanBeFetched()
+        {
+            var history = new VialSetHistory();
+
+            history.Store(new VialSet() { Vials = new Vial[] { new Vial(new Color[] { Color.Red }) } });
+            history.Store(new VialSet() { Vials = new Vial[] { new Vial(new Color[] { Color.Red, Color.Blue }) } });
+            history.Store(new VialSet() { Vials = new Vial[] { new Vial(new Color[] { Color.Red, Color.Blue, Color.Green }) } });
+
+            history.GetPrevious();
+            history.GetPrevious();
+
+            history.Current.Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red });
+
+            history.GetNext().Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red, Color.Blue });
+            history.Current.Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red, Color.Blue });
+
+            history.GetNext().Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red, Color.Blue, Color.Green });
+            history.Current.Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red, Color.Blue, Color.Green });
+
+            history.GetNext().ShouldBeNull();
+            history.Current.Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red, Color.Blue, Color.Green });
+        }
+
+        [TestMethod]
+        public void StoringErasesNextOnes()
+        {
+            var history = new VialSetHistory();
+
+            history.Store(new VialSet() { Vials = new Vial[] { new Vial(new Color[] { Color.Red }) } });
+            history.Store(new VialSet() { Vials = new Vial[] { new Vial(new Color[] { Color.Red, Color.Blue }) } });
+            history.Store(new VialSet() { Vials = new Vial[] { new Vial(new Color[] { Color.Red, Color.Blue, Color.Green }) } });
+
+            history.GetPrevious();
+            history.GetPrevious();
+
+            history.Current.Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Red });
+
+            history.Store(new VialSet() { Vials = new Vial[] { new Vial(new Color[] { Color.Green, Color.Yellow }) } });
+
+            history.Current.Vials.Single().Colors.ToArray().ShouldBe(new Color[] { Color.Green, Color.Yellow });
+            history.GetNext().ShouldBeNull();
+        }
+    }
+}
---
+++ Vials/Shared/VialSetHistory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vials.Shared
+{
+    public class VialSetHistory : IVialSetHistory
+    {
+        private readonly IList<VialSet> vialSets = new List<VialSet>();
+        private int currentIndex = -1;
+
+        public VialSet Current 
+        {
+            get
+            {
+                if(currentIndex >= 0)
+                {
+                    return vialSets[currentIndex];
+                }
+
+                return null;
+            }
+        }
+
+        public void Store(VialSet set)
+        {
+            ClearUpcoming();
+
+            vialSets.Add(set);
+            currentIndex++;
+        }
+
+        public VialSet GetPrevious()
+        {
+            if(currentIndex > 0)
+            {
+                currentIndex--;
+                return Current;
+            }
+
+            return null;
+        }
+
+        public VialSet GetNext()
+        {
+            if(currentIndex < vialSets.Count - 1)
+            {
+                currentIndex++;
+                return Current;
+            }
+
+            return null;
+        }
+
+        private void ClearUpcoming()
+        {
+            if (currentIndex < 0)
+            {
+                return;
+            }
+
+            while (currentIndex + 1 < vialSets.Count)
+            {
+                vialSets.RemoveAt(currentIndex + 1);
+            }
+        }
+    }
+}
