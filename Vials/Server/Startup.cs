7791030d74c44e57ce1832639c27242609b1bb6e
Mikael Lindh <mslindh@gmail.com>
2021-06-29 22:25:04 +0200

    Goodbye Core.

--- Vials/Server/Startup.cs
+++ Vials/Server/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
-using Vials.Core;
 
 namespace Vials.Server
 {
