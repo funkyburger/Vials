878c46642f97a5111649be3528adb27d66a3699c
Mikael Lindh <mslindh@gmail.com>
2021-07-06 23:05:37 +0200

    Added access to page cookies.

---
+++ Vials/Client/Utilities/CookieAccess.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vials.Client.Utilities
+{
+    public class CookieAccess : ICookieAccess
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public CookieAccess(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task<string> Get()
+        {
+            return await _jsRuntime.InvokeAsync<string>("eval", new object[] { $"document.cookie" });
+        }
+
+        public async Task Set(string value)
+        {
+            await _jsRuntime.InvokeAsync<string>("eval", new object[] { $"document.cookie = '{ value }'" });
+        }
+    }
+}
