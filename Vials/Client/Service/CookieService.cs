c2c74bc86ed07db100862c704fb4cd9ac82da08e
Mikael Lindh <mslindh@gmail.com>
2021-07-07 09:46:54 +0200

    Added cookie service.

---
+++ Vials/Client/Service/CookieService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vials.Client.Utilities;
+using Vials.Shared.Objects;
+
+namespace Vials.Client.Service
+{
+    public class CookieService : ICookieService
+    {
+        private readonly ICookieStore _cookieStore;
+
+        public CookieService(ICookieStore cookieStore)
+        {
+            _cookieStore = cookieStore;
+        }
+
+        public async Task<bool> DidUserConsent()
+        {
+            var cookie = await _cookieStore.Get();
+
+            return cookie != null;
+        }
+
+        public async Task MarkUserDidConsent()
+        {
+            await _cookieStore.Store(new ApplicationCookie());
+        }
+    }
+}
---
+++ Vials/Client/Service/ICookieService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vials.Shared.Objects;
+
+namespace Vials.Client.Service
+{
+    public interface ICookieService
+    {
+        Task<bool> DidUserConsent();
+        Task MarkUserDidConsent();
+    }
+}
