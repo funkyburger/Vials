c04527c50c8c99592a62f8766b771fc90d505e0d
Mikael Lindh <mslindh@gmail.com>
2021-07-19 10:24:44 +0200

    Added tooltip for number box.

---
+++ Vials/Client/CodeBehind/Tooltip.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vials.Client.CodeBehind
+{
+    public class Tooltip : VialComponentBase
+    {
+        [Parameter] 
+        public RenderFragment ChildContent { get; set; }
+        [Parameter] 
+        public string Text { get; set; }
+    }
+}
