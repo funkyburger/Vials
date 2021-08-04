1139fd122fa93f9cb3252ea92585a20d4de65520
Mikael Lindh <mslindh@gmail.com>
2021-08-04 15:56:23 +0200

    Added bit mirror for long.

---
+++ Vials.Shared.UnitTests/Extensions/LongExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Vials.Shared.Extensions;
+
+namespace Vials.Shared.UnitTests.Extensions
+{
+    [TestClass]
+    public class LongExtensionsTests
+    {
+        [TestMethod]
+        public void MirrorsWork()
+        {
+            var number = (long)0b0001_0101_0011_1111_0000_1110_0010_0001; // 356453921
+            number.MirrorBytes().ShouldBe(0b0000_0100_0111_0000_1111_1100_1010_1000_0001_0101_0011_1111_0000_1110_0010_0001);
+        }
+
+        [TestMethod]
+        public void MirrorOfBigOneIsSame()
+        {
+            var number = 0b0001_0001_0101_0011_1111_0000_1110_0010_0001; // Too big
+            number.MirrorBytes().ShouldBe(number);
+            number = 0b1001_0101_0011_1111_0000_1110_0010_0001; // Almost
+            number.MirrorBytes().ShouldNotBe(number);
+        }
+    }
+}
---
+++ Vials/Shared/Extensions/LongExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vials.Shared.Extensions
+{
+    public static class LongExtensions
+    {
+        public static long MirrorBytes(this long input)
+        {
+            if(input >> 32 > 0)
+            {
+                return input;
+            }
+
+            var result = 0L;
+            var temp = input;
+            temp >>= 1; // first bit would overwrite sign bit.
+
+            for(int i = 0; i < 63; i++)
+            {
+                result += temp % 2;
+
+                temp >>= 1;
+                result <<= 1;
+            }
+
+            result >>= 1;
+
+            return result ^ input;
+        }
+    }
+}
