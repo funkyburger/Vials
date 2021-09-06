2c391c9b6ea2e442c59f0857baa95902693e4c79
Mikael Lindh <mslindh@gmail.com>
2021-09-06 15:35:04 +0200

    Fixed chronology and faulty track.

--- Vials/Client/Utilities/MoveTracker.cs
+++ Vials/Client/Utilities/MoveTracker.cs
@@ -2,23 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Vials.Shared.Extensions;
 
 namespace Vials.Client.Utilities
 {
     public class MoveTracker : IMoveTracker
     {
+        // TODO support of backtracking
+
         private readonly IList<long> _stack = new List<long>();
 
         public void Stack(long fromFootPrint, long toFootPrint, long ts)
         {
-            _stack.Add(ts);
-            _stack.Add(toFootPrint);
+            Console.WriteLine("tracker : " + ts.ToTimeString());
+
             _stack.Add(fromFootPrint);
+            _stack.Add(toFootPrint);
+            _stack.Add(ts);
         }
 
         public IEnumerable<long> GetStack()
         {
-            return _stack.Reverse();
+            return _stack;
         }
     }
 }
