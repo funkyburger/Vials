6063631a3d5dcba1c6393fd12e332d201fb51e1d
Mikael Lindh <mslindh@gmail.com>
2021-09-06 14:27:46 +0200

    Made chronology check.

---
+++ Vials.Server.UnitTests/Game/Check/ChronologyCheckTests.cs
@@ -0,0 +1,58 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Vials.Shared.Objects;
+using Vials.Server.Utilities;
+using Vials.Shared;
+using Vials.Server.UnitTests.Utils;
+using Vials.Server.Game.Check;
+using Vials.Shared.Exceptions;
+using Vials.Server.Game;
+using Moq;
+
+namespace Vials.Server.UnitTests.Game.Check
+{
+    public class ChronologyCheckTests
+    {
+        [Test]
+        public async Task GoodChronologyIsValidated()
+        {
+            var finishedGameHelper = new Mock<IFinishedGameHelper>();
+            finishedGameHelper.Setup(h => h.ExtractTimestamps(It.IsAny<FinishedGame>()))
+                .Returns(new long[] { 789, 790, 791 });
+
+            var check = new ChronologyCheck(finishedGameHelper.Object);
+
+            check.Check(new FinishedGame());
+        }
+
+        [Test]
+        public async Task BadChronologyThrowsAnException()
+        {
+            var finishedGameHelper = new Mock<IFinishedGameHelper>();
+            finishedGameHelper.Setup(h => h.ExtractTimestamps(It.IsAny<FinishedGame>()))
+                .Returns(new long[] { 789, 792, 791 });
+
+            var check = new ChronologyCheck(finishedGameHelper.Object);
+            
+            Assert.Throws<GameCheckException>(() => check.Check(new FinishedGame()));
+        }
+
+        [Test]
+        public async Task SimulataneousActionsThrowsAnException()
+        {
+            var finishedGameHelper = new Mock<IFinishedGameHelper>();
+            finishedGameHelper.Setup(h => h.ExtractTimestamps(It.IsAny<FinishedGame>()))
+                .Returns(new long[] { 789, 790, 790 });
+
+            var check = new ChronologyCheck(finishedGameHelper.Object);
+            
+            Assert.Throws<GameCheckException>(() => check.Check(new FinishedGame()));
+        }
+    }
+}
--- Vials/Server/Game/Check/ChronologyCheck.cs
+++ Vials/Server/Game/Check/ChronologyCheck.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Vials.Shared.Exceptions;
 using Vials.Shared.Objects;
 
 namespace Vials.Server.Game.Check
 {
     public class ChronologyCheck : IGameCheck
     {
+        private readonly IFinishedGameHelper _finishedGameHelper;
+
+        public ChronologyCheck(IFinishedGameHelper finishedGameHelper)
+        {
+            _finishedGameHelper = finishedGameHelper;
+        }
+
         public void Check(FinishedGame game)
         {
-            throw new NotImplementedException();
+            long ts = 0;
+
+            foreach(var timestamp in _finishedGameHelper.ExtractTimestamps(game))
+            {
+                if(timestamp <= ts)
+                {
+                    throw new GameCheckException("Timestamps are not in order.");
+                }
+
+                ts = timestamp;
+            }
         }
     }
 }
