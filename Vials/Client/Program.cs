c14d4d659267e073eeee0dec6d956ca4b339b49c
Mikael Lindh <mslindh@gmail.com>
2021-09-07 16:44:26 +0200

    Put modal dialog backbone.

--- Vials/Client/Program.cs
+++ Vials/Client/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Modal;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -25,6 +26,8 @@ namespace Vials.Client
 
         private static void ConfigureServices(WebAssemblyHostBuilder builder)
         {
+            builder.Services.AddBlazoredModal();
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<Utilities.IColorToCssMapper, Utilities.ColorToCssMapper>();
             builder.Services.AddScoped<Utilities.ICookieAccess, Utilities.CookieAccess>();
